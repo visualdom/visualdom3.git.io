fb2f9f488aaa6a91881d69dda36c33b117f1ba71
IDS Comercial <cesarapolinara@gmail.com>
2019-04-08 17:17:39 -0500

    abril8

---
+++ java/e-reservation/src/main/java/com/platzi/ereservation/modelo/Cliente.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package com.platzi.ereservation.modelo;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import lombok.Data;
+
+/**
+ * Clase que representa la tabla Cliente
+ * @author idscomercial
+ *
+ */
+@Data
+@Entity 
+@Table(name = "cliente")
+public class Cliente {
+	@Id
+	@GeneratedValue(generator = "system-uuid")
+	@GenericGenerator(name ="system-uuid", strategy ="uuid2")
+	private String idCli;
+	private String nombreCli;
+	private String apellidoCli;
+	private String identificacionCli;
+	private String direccionCli;
+	private String telefonoCli;
+	private String emailCli;
+	@OneToMany(mappedBy="cliente")
+	private Set<Reserva> reservas;
+	
+	public Cliente() {
+		// Todo Aut-generatedconstructor stub
+	}
+	
+
+}
---
+++ java/e-reservation/src/main/java/com/platzi/ereservation/modelo/Reserva.java
@@ -0,0 +1,38 @@
+
+package com.platzi.ereservation.modelo;
+
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import lombok.Data;
+
+/**
+ * Clase que representa la tabla reserva
+ * @author idscomercial
+ *
+ */
+@Data
+@Entity
+@Table(name="reserva")
+public class Reserva {
+	@Id
+	@GeneratedValue(generator = "system-uuid")
+	@GenericGenerator(name ="system-uuid", strategy ="uuid2")
+	private String idRes;
+	private Date fechaIngresoRes;
+	private Date fechaSalidaRes;
+	private int cantidadPersonasRes;
+	private String descripcionRes;
+	@ManyToOne
+	@JoinColumn(name="idCli")
+	private Cliente cliente;
+}
